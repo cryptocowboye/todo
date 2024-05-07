76b6804ace57e26bb16fd33aefeeb68e7776da32
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2024-05-07 00:01:17 -0400

    Add a functional default project

--- src/storageChecker.js
+++ src/storageChecker.js
@@ -1,6 +1,14 @@
+import Project from "./Project"
+
 export default function storageChecker (arr) {
     if (arr === null) {
-        arr = []
+        const defaultProj = new Project("Default Project", -1);
+        const createdTodo1 = defaultProj.createToDo("todo1", "Write a description here", "2020-1-1", "red", defaultProj.todoIndex);
+        defaultProj.addToDo(createdTodo1);
+        const createdTodo2 = defaultProj.createToDo("todo2", "Write a description here", "2020-1-2", "green", defaultProj.todoIndex);
+        defaultProj.addToDo(createdTodo2);
+
+        arr = {projects: [defaultProj]}
     }
 
     return arr
