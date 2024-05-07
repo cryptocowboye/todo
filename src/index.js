e67411ca72fec7903aa49760b6b213f2fe7ad54e
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2024-05-07 00:03:41 -0400

    Reconstruct projects from storage

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import List from './List'
 import storageChecker from './storageChecker'
 import indexChecker from './indexChecker'
 import modals from './modals'
+import remover from './remover'
 
 
 
 
-const storageArr = storageChecker(JSON.parse(localStorage.getItem("appArray")));
+const storageArr = storageChecker(JSON.parse(localStorage.getItem("appStorage")));
 const indexNum = indexChecker(JSON.parse(localStorage.getItem("index")));
-const listOfProjects = new List(storageArr, indexNum);
+const listOfProjects = new List(storageArr.projects, indexNum);
 
 
 function page () {
     const projectForm = document.querySelector(".project-form > form");
     const projectTitle = document.querySelector("#project-title");
+
+
 
     projectForm.addEventListener("submit", (e) => {
         const project = listOfProjects.createProject(projectTitle.value)
@@ -25,6 +28,16 @@ function page () {
     })
 
     modals()
+
+    if (listOfProjects.projects.length > 0) {
+        listOfProjects.setIndex();
+        
+        listOfProjects.projects.forEach((project) => {
+            const reconstructedProj = listOfProjects.reconstructProject(project);
+            listOfProjects.addProjectElement(reconstructedProj);
+            remover()
+        })
+    }
 }
 
 page()
