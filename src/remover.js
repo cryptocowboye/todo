108c76d05f939eab1506d453492f6f4947982eee
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2024-05-08 00:49:06 -0400

    Add filler content

--- src/remover.js
+++ src/remover.js
@@ -1,7 +1,19 @@
-export default function remover () {
+export default function remover (isEmpty) {
     const mainContent = document.querySelector("main");
     if (mainContent.hasChildNodes()) {
         const child = document.querySelector(".todo-container");
         mainContent.removeChild(child);
+    }
+
+    if (isEmpty) {
+        const child = document.createElement("div");
+        child.classList.add("todo-container");
+        child.textContent = "Click or create a project!"
+
+        child.style.fontSize = "100px";
+        child.style.justifyContent = "center";
+        child.style.alignItems = "center";
+
+        mainContent.append(child)
     }
 }
