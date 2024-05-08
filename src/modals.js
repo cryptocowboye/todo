9cf32bb95ad2e3859971f088d671a858867726af
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2024-05-08 00:58:33 -0400

    Remove initial todo dialog functionality

--- src/modals.js
+++ src/modals.js
@@ -3,13 +3,7 @@ export default function modals () {
     const createNewCloseBtn = document.querySelector("#close-project-modal");
     const projectDialog = document.querySelector(".project-form");
 
-    const createTodoBtn = document.querySelector(".create-todo");
-    const closeTodoBtn = document.querySelector("#close-todo-modal");
-    const todoDialog = document.querySelector("#todo-form");
-
     createNewProjBtn.addEventListener("click", () => projectDialog.show());
     createNewCloseBtn.addEventListener("click", () => projectDialog.close());
     
-    createTodoBtn.addEventListener("click", () => todoDialog.show());
-    closeTodoBtn.addEventListener("click", () => todoDialog.close());
 }
